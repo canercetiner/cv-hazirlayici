a81fcfdab217692fec0e5e6847c28193e70497de
Caner Cetiner <caner.cetiner@icloud.com>
2026-02-09 00:18:20 +0300

    fix: Remove @iabtcf/core and simplify metro config

--- metro.config.js
+++ metro.config.js
@@ -5,23 +5,4 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts.push("mjs");
 
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-    if (moduleName === '@iabtcf/core') {
-        return {
-            filePath: require.resolve('@iabtcf/core/lib/cjs/index.js'),
-            type: 'sourceFile',
-        };
-    }
-
-    // Mock react-native-google-mobile-ads for web platform
-    if (platform === 'web' && moduleName === 'react-native-google-mobile-ads') {
-        return {
-            filePath: require.resolve('./node_modules/react-native-google-mobile-ads/lib/commonjs/index.js'),
-            type: 'sourceFile',
-        };
-    }
-
-    return context.resolveRequest(context, moduleName, platform);
-};
-
 module.exports = withNativeWind(config, { input: "./global.css" });
